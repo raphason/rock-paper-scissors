dcac1ae6608c4926e72dacfb8f71ab155b676af8
Raphael Son <raphaelson2000@yahoo.com>
2022-10-23 20:22:35 -0400

    Add playRound function

--- game.js
+++ game.js
@@ -17,3 +17,44 @@ function getComputerChoice() {
 
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    //Compare player and computer selections and declare round winner
+    playerSelection = capitalize(playerSelection);
+
+    const playerWin = "You Win! " + playerSelection + " beats " + computerSelection + "!";
+    const computerWin = "You Lose! " + computerSelection + " beats " + playerSelection + "!";
+    const tie = "It's a tie! You both chose " + playerSelection + "!";
+
+    if (playerSelection == computerSelection) {
+        return tie;
+    }
+    switch (playerSelection) {
+        case "Rock":
+            if (computerSelection == "Scissors") {
+                return playerWin;
+            }
+            else {
+                return computerWin;
+            }
+        case "Paper":
+            if (computerSelection == "Rock") {
+                return playerWin;
+            }
+            else {
+                return computerWin;
+            }
+        case "Scissors":
+            if (computerSelection == "Paper") {
+                return playerWin;
+            }
+            else {
+                return computerWin;
+            }
+    }
+}
+
+function capitalize(str) {
+    result = str.slice(0, 1).toUpperCase() + str.slice(1, str.length).toLowerCase()
+    return result;
+}
